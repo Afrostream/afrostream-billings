865906683e0a1a1a5ed6bcadda244483dc987768
nelsounet <nelsounet@afrostream.tv>
2016-01-12 17:34:10 +0100

    fix : adding user on same provider with 2 different userProviderUuids

--- libs/providers/bachat/users/BachatUsersHandler.php
+++ libs/providers/bachat/users/BachatUsersHandler.php
@@ -13,27 +13,13 @@ class BachatUsersHandler {
 		try {
 			config::getLogger()->addInfo("bachat user creation...");
 			if(isset($user_provider_uuid)) {
-				//TODO
+				//nothing
+				//TODO : (should check provider side...)
 			} else {
-				//
-				if(!isset($user_opts_array['email'])) {
-					$msg = "userOpts field 'email' was not provided";
-					config::getLogger()->addError($msg);
-					throw new BillingsException(new ExceptionType(ExceptionType::internal), $msg);
-				}
-				if(!isset($user_opts_array['firstName'])) {
-					$msg = "userOpts field 'firstName' was not provided";
-					config::getLogger()->addError($msg);
-					throw new BillingsException(new ExceptionType(ExceptionType::internal), $msg);
-				}
-				if(!isset($user_opts_array['lastName'])) {
-					$msg = "userOpts field 'lastName' was not provided";
-					config::getLogger()->addError($msg);
-					throw new BillingsException(new ExceptionType(ExceptionType::internal), $msg);
-				}
-				//TODO
+				$msg = "unsupported feature for provider named bachat";
+				config::getLogger()->addError($msg);
+				throw new BillingsException(new ExceptionType(ExceptionType::internal), $msg);
 			}
-			//TODO : $user_provider_uuid = 
 			config::getLogger()->addInfo("bachat user creation done successfully, user_provider_uuid=".$user_provider_uuid);
 		} catch(BillingsException $e) {
 			$msg = "a billings exception occurred while creating a bachat user for user_reference_uuid=".$user_reference_uuid.", error_code=".$e->getCode().", error_message=".$e->getMessage();
