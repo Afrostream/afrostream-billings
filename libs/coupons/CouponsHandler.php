483e2177a012b9cda0c804c09d1cb2f6861f2142
Stéphane Reuille <sreuille@sapiensgroup.fr>
2016-06-17 11:26:47 +0200

    Add class StripeCouponsHandler

--- libs/coupons/CouponsHandler.php
+++ libs/coupons/CouponsHandler.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../db/dbGlobal.php';
 require_once __DIR__ . '/../providers/afr/coupons/AfrCouponsHandler.php';
 require_once __DIR__ . '/../providers/cashway/coupons/CashwayCouponsHandler.php';
+require_once __DIR__ . '/../providers/stripe/coupons/StripeCouponsHandler.php';
 
 class CouponsHandler {
 	
@@ -112,6 +113,10 @@ class CouponsHandler {
 					$cashwayCouponsHandler = new CashwayCouponsHandler();
 					$coupon_provider_uuid = $cashwayCouponsHandler->doCreateCoupon($user, $userOpts, $couponsCampaign, $coupon_billing_uuid);
 					break;
+				case 'stripe' :
+					$stripeCouponsHandler = new StripeCouponsHandler();
+					$coupon_provider_uuid = $stripeCouponsHandler->doCreateCoupon($user, $userOpts, $couponsCampaign, $coupon_billing_uuid);
+					break;
 				default :
 					$msg = "unsupported feature for provider named : ".$provider->getName();
 					config::getLogger()->addError($msg);
@@ -122,6 +127,10 @@ class CouponsHandler {
 				case 'cashway' :
 					$cashwayCouponsHandler = new CashwayCouponsHandler();
 					$db_coupon = $cashwayCouponsHandler->createDbCouponFromApiCouponUuid($user, $userOpts, $couponsCampaign, $coupon_billing_uuid, $coupon_provider_uuid);
+					break;
+				case 'stripe' :
+					$stripeCouponsHandler = new StripeCouponsHandler();
+					$db_coupon = $stripeCouponsHandler->createDbCouponFromApiCouponUuid($user, $userOpts, $couponsCampaign, $coupon_billing_uuid, $coupon_provider_uuid);
 					break;
 				default :
 					$msg = "unsupported feature for provider named : ".$provider->getName();
