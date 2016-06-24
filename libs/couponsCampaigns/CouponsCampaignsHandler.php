93dec5dbc8d09fe8172d1b2f61e2dabdf35c73b3
nelsounet <nelsounet@afrostream.tv>
2016-06-24 17:01:05 +0200

    get one couponCampaign

--- libs/couponsCampaigns/CouponsCampaignsHandler.php
+++ libs/couponsCampaigns/CouponsCampaignsHandler.php
@@ -36,6 +36,26 @@ class CouponsCampaignsHandler {
 		return($db_coupons_campaigns);
 	}
 	
+	public function doGetCouponsCampaign($couponsCampaignBillingUuid) {
+		$db_coupons_campaign = NULL;
+		try {
+			config::getLogger()->addInfo("CouponsCampaign getting, couponsCampaignBillingUuid=".$couponsCampaignBillingUuid."....");
+			//
+			$db_coupons_campaign = CouponsCampaignDAO::getCouponsCampaignByUuid($couponsCampaignBillingUuid);
+			//
+			config::getLogger()->addInfo("CouponsCampaign getting, couponsCampaignBillingUuid=".$couponsCampaignBillingUuid." done successfully");
+		} catch(BillingsException $e) {
+			$msg = "a billings exception occurred while getting a CouponsCampaign for couponsCampaignBillingUuid=".$couponsCampaignBillingUuid.", error_code=".$e->getCode().", error_message=".$e->getMessage();
+			config::getLogger()->addError("CouponsCampaign getting failed : ".$msg);
+			throw $e;
+		} catch(Exception $e) {
+			$msg = "an unknown exception occurred while getting a CouponsCampaign for couponsCampaignBillingUuid=".$couponsCampaignBillingUuid.", error_code=".$e->getCode().", error_message=".$e->getMessage();
+			config::getLogger()->addError("CouponsCampaign getting failed : ".$msg);
+			throw new BillingsException(new ExceptionType(ExceptionType::internal), $msg);
+		}
+		return($db_coupons_campaign);
+	}
+	
 }
 
 ?>
