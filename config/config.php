104b57b390042fbc060547d3c415eee453aea6a2
nelsounet <nelsounet@afrostream.tv>
2016-12-06 15:27:04 +0100

    wecashup config update

--- config/config.php
+++ config/config.php
@@ -396,23 +396,19 @@ if(getEnv('CONTEXTS_SWITCH_EXPIRED_DATE_BOUNDARY_TO_COMMON_CONTEXT') === false) 
 #Wecashup
 
 if(getEnv('WECASHUP_MERCHANT_UID') === false) {
-	putEnv('WECASHUP_MERCHANT_UID=nZpRMzyIg2Tgh7o2nIK2heGkfVB2');
+	putEnv('WECASHUP_MERCHANT_UID=bzmSSCP8WqUMDDH4sPb2w8hB14F2');
 }
 
 if(getEnv('WECASHUP_MERCHANT_PUBLIC_KEY') === false) {
-	putEnv('WECASHUP_MERCHANT_PUBLIC_KEY=SM3xFy5l31t9HD5XuosCfNuhZvNNhEQHNexLu4VjPtu3');
+	putEnv('WECASHUP_MERCHANT_PUBLIC_KEY=NoZ7voE0KDRSBnxaB7oqcGdWQnrLVxAZm9NLiEIMyYvq');
 }
 
 if(getEnv('WECASHUP_MERCHANT_SECRET') === false) {
-	putEnv('WECASHUP_MERCHANT_SECRET=UGdE8slkMh434lhZ');
-}
-
-if(getEnv('WECASHUP_MERCHANT_SECRET') === false) {
-	putEnv('WECASHUP_MERCHANT_SECRET=UGdE8slkMh434lhZ');
+	putEnv('WECASHUP_MERCHANT_SECRET=PwWqkwwq8L7nlb61');
 }
 
 if(getEnv('WECASHUP_API_URL') === false) {
-	putEnv('WECASHUP_API_URL=https://merchant-dashboard-dot-wecashup-payment.appspot.com/api/v1.0/merchants');
+	putEnv('WECASHUP_API_URL=https://www.wecashup.com/api/v1.0/merchants');
 }
 
 #logger, #db_conn, ...
