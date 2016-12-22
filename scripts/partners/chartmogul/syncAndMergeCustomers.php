184dfc65df04d093529d6e6fa6da0d962c2e2318
nelsounet <nelsounet@afrostream.tv>
2016-12-22 10:39:31 +0100

    add chartmogul SyncAndMergeCustomers script

--- scripts/partners/chartmogul/syncAndMergeCustomers.php
+++ scripts/partners/chartmogul/syncAndMergeCustomers.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../../db/dbGlobal.php';
 require_once __DIR__ . '/../../libs/partners/chartmogul/BillingsChartmogulWorkers.php';
