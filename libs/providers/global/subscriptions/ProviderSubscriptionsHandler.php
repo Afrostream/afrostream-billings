03fd24237266959143cf8b0e08a4a8c3e0044879
nelsounet <nelsounet@afrostream.tv>
2017-02-02 10:45:35 +0100

    send grid lib changed

--- libs/providers/global/subscriptions/ProviderSubscriptionsHandler.php
+++ libs/providers/global/subscriptions/ProviderSubscriptionsHandler.php
@@ -480,7 +480,7 @@ class ProviderSubscriptionsHandler {
 			}
 			$mail->addPersonalization($personalization);
 			$mail->setTemplateId($sendgrid_template_id);
-			$mail->setSubject('');
+			$mail->setSubject('Here goes the subject');
 			$mail->addContent(new SendGrid\Content('text/html', '<html></html>'));
 			$json_string = json_encode($mail, JSON_PRETTY_PRINT);
 			config::getLogger()->addInfo("mail to be sent : ".$json_string);
