38a3c798b79b5f8538851e4d40e632b4ac91c64e
nelsounet <nelsounet@afrostream.tv>
2017-03-08 11:22:06 +0100

    script refactoring

--- scripts/providers/afr/exportAfrSubscriptions.php
+++ scripts/providers/afr/exportAfrSubscriptions.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../../db/dbGlobal.php';
+require_once __DIR__ . '/../../../libs/db/dbGlobal.php';
 require_once __DIR__ . '/../../libs/providers/afr/BillingsExportAfrSubscriptionsWorkers.php';
 
 /*
