e36e163ec6c8b65a65f86e2ba948577c361a20fe
nelsounet <nelsounet@afrostream.tv>
2017-03-17 15:39:36 +0100

    add expireSubscriptions script

---
+++ scripts/expireSubscriptions.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once __DIR__ . '/../libs/db/dbGlobal.php';
+
+/*
+ * Tool to import users from Afrostream DB
+ */
+
+print_r("starting tool to expire afrostream subscriptions from a given plan...\n");
+
+foreach ($argv as $arg) {
+	$e=explode("=",$arg);
+	if(count($e)==2)
+		$_GET[$e[0]]=$e[1];
+		else
+			$_GET[$e[0]]=0;
+}
+
+$platform = BillingPlatformDAO::getPlatformById(1);
+
+$internalPlanUuid = NULL;
+
+if(isset($_GET["-internalPlanUuid"])) {
+	$internalPlanUuid = $_GET["-internalPlanUuid"];
+} else {
+	print_r("-internalPlanUuid is missing\n");
+	exit;
+}
+
+$internalPlan = InternalPlanDAO::getInternalPlanByUuid($internalPlanUuid, $platform->getId());
+
+if($internalPlan == NULL) {
+	print_r("internalPlan with internalPlanUuid=".$internalPlanUuid." does not exist\n");
+	exit;
+}
+
+print_r("processing...\n");
+
+$query =<<<EOL
+SELECT count(*) OVER() as total_counter, 
+BU._id as _id, 
+(CASE WHEN length(BUO.value) = 0 THEN 'unknown@domain.com' ELSE BUO.value END) as email,
+BS.sub_uuid
+FROM billing_subscriptions BS
+INNER JOIN
+billing_plans BP ON (BP._id = BS.planid)
+INNER JOIN
+billing_internal_plans_links BIPL ON (BIPL.provider_plan_id = BP._id)
+INNER JOIN
+billing_internal_plans BIP ON (BIP._id = BIPL.internal_plan_id)
+INNER JOIN
+billing_users BU ON (BS.userid = BU._id)
+LEFT JOIN 
+billing_users_opts BUO ON (BUO.userid = BU._id AND BUO.key = 'email' AND BUO.deleted = 'no')
+WHERE BS.deleted = false 
+AND BU.deleted = false 
+AND BS.sub_activated_date <= CURRENT_TIMESTAMP
+AND (BS.sub_expires_date IS NULL OR BS.sub_expires_date > CURRENT_TIMESTAMP) 
+AND BU.platformid = 1 AND BIP.internal_plan_uuid = '%s'
+ORDER BY BU._id ASC
+EOL;
+
+$query = sprintf($query, $internalPlanUuid);
+$limit = 1000;
+$offset = 0;
+
+do {
+	$result = dbGlobal::loadSqlResult($query, $limit, $offset);
+	$offset = $offset + $limit;
+	if(is_null($totalCounter)) {$totalCounter = $result['total_counter'];}
+	$idx+= count($result['rows']);
+	$lastId = $result['lastId'];
+	//
+	foreach($result['rows'] as $row) {
+		print_r("sub_uuid=".$row['sub_uuid']."\n");
+	}
+} while ($idx < $totalCounter && count($result['rows']) > 0);
+
+print_r("processing done\n");
+
+?>
