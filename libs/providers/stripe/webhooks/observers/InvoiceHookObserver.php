1d7402d3034796bdb325fd37ece95e9451b827d8
nelsounet <nelsounet@afrostream.tv>
2017-05-22 15:08:31 +0200

    add next_payment_attempt parameter testing

--- libs/providers/stripe/webhooks/observers/InvoiceHookObserver.php
+++ libs/providers/stripe/webhooks/observers/InvoiceHookObserver.php
@@ -39,25 +39,34 @@ class InvoiceHookObserver implements HookInterface
 	    	$api_invoice_id = $event['data']['object']['id'];
 	    	config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' api_invoice_id='.$api_invoice_id);
 	    	$api_invoice = \Stripe\Invoice::retrieve($api_invoice_id);
-	    	$api_subscription = NULL;
-	    	if(isset($api_invoice['subscription'])) {
-	    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' api_subscription_id='.$api_invoice['subscription']);
-	    		$api_subscription = \Stripe\Subscription::retrieve($api_invoice['subscription']);
-	    	} else {
-	    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' no api_subscription_id found');
+	    	$hasNextPaymentAttempt = false;
+	    	if(isset($api_invoice['next_payment_attempt'])) {
+	    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' next_payment_attempt='.$api_invoice['next_payment_attempt']);
+	    		$hasNextPaymentAttempt = true;
 	    	}
-	    	$billingSubscription = NULL;
-	    	if(isset($api_subscription)) {
-	    		$billingSubscription = BillingsSubscriptionDAO::getBillingsSubscriptionBySubUuid($provider->getId(), $api_subscription['id']);
+	    	if($hasNextPaymentAttempt) {
+		    	$api_subscription = NULL;
+		    	if(isset($api_invoice['subscription'])) {
+		    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' api_subscription_id='.$api_invoice['subscription']);
+		    		$api_subscription = \Stripe\Subscription::retrieve($api_invoice['subscription']);
+		    	} else {
+		    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' no api_subscription_id found');
+		    	}
+		    	$billingSubscription = NULL;
+		    	if(isset($api_subscription)) {
+		    		$billingSubscription = BillingsSubscriptionDAO::getBillingsSubscriptionBySubUuid($provider->getId(), $api_subscription['id']);
+		    	} else {
+		    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' no api_subscription found');
+		    	}
+		    	if(isset($billingSubscription)) {
+		    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' billing_subscription_uuid='.$billingSubscription->getSubscriptionBillingUuid());
+		    		$providerSubscriptionsHandlerInstance = ProviderHandlersBuilder::getProviderSubscriptionsHandlerInstance($provider);
+		    		$providerSubscriptionsHandlerInstance->doSendSubscriptionEvent($billingSubscription, $billingSubscription, 'FAILED_PAYMENT');
+		    	} else {
+		    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' no billingSubscription found');
+		    	}
 	    	} else {
-	    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' no api_subscription found');
-	    	}
-	    	if(isset($billingSubscription)) {
-	    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' billing_subscription_uuid='.$billingSubscription->getSubscriptionBillingUuid());
-	    		$providerSubscriptionsHandlerInstance = ProviderHandlersBuilder::getProviderSubscriptionsHandlerInstance($provider);
-	    		$providerSubscriptionsHandlerInstance->doSendSubscriptionEvent($billingSubscription, $billingSubscription, 'FAILED_PAYMENT');
-	    	} else {
-	    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' no billingSubscription found');
+	    		config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' no next_payment_attempt found');
 	    	}
 	    	//
 	    	config::getLogger()->addInfo('STRIPE - Process new event id='.$event['id'].', type='.$event['type'].' has been processed successfully');
